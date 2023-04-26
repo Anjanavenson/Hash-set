88d9c38c5a7a4fa81ee00d448fbc54066d0fc834
anjana <anjana.v@eminds.ai>
2023-04-26 23:55:14 +0530

    Hashing

---
+++ SetDemo.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        HashSet hs = new HashSet();
+        hs.add(5);
+        hs.add(100);
+        hs.add(50);
+        hs.add(6);
+        System.out.println(hs.add(5));
+        System.out.println(hs);
+    }
+}
